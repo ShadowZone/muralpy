030400fa27dc5ec47436f8000471b33fb57d1618
ShadowZone <ShadowZone@users.noreply.github.com>
2023-02-24 19:59:29 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ShadowZone"
+original_name="muralpy"
+original_urlname="muralpy"
+original_description="Awesome muralpy created by ShadowZone"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv muralpy $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- muralpy/__main__.py
+++ muralpy/__main__.py
@@ -1,4 +1,4 @@
-"""Entry point for project_name."""
+"""Entry point for muralpy."""
 
 from .cli import main  # pragma: no cover
 
